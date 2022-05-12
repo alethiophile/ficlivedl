c0a3ed3a519bce341a400d961c51c5de457e9506
alethiophile <mail@alethiophile.dev>
2022-05-12 14:13:34 -0600

    build system

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ if [[ $1 == "dev" ]]; then
     node_modules/.bin/browserify --ignore archiver webextension/background.js >webextension/browser_pack.js
 else
     node_modules/.bin/browserify --ignore archiver  webextension/background.js --plugin tinyify >webextension/browser_pack.js
-    web-ext build -s webextension/ -i jszip.js -i background.js
+    web-ext build -s webextension/ -i jszip.js -i background.js --overwrite-dest
 fi
