4ff3a2c41202092bab60b2ac902836c33b41a3f3
alethiophile <mail@alethiophile.dev>
2022-05-12 13:25:06 -0600

    Added tinyify for production

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-node_modules/.bin/browserify webextension/background.js >webextension/browser_pack.js
+if [[ $1 == "dev" ]]; then
+    node_modules/.bin/browserify webextension/background.js >webextension/browser_pack.js
+else
+    node_modules/.bin/browserify webextension/background.js --plugin tinyify >webextension/browser_pack.js
+fi
