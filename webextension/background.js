37bfc3a29088d68282484634e66d030fbef4ff85
alethiophile <mail@alethiophile.dev>
2022-05-12 13:25:06 -0600

    Updates, first usable version

--- webextension/background.js
+++ webextension/background.js
@@ -15,7 +15,28 @@ let downloadState = null;
 
 HtmlSanitizer.AllowedTags['FIGURE'] = true;
 
-function Story(node_id) {
+function url_basename(url) {
+    let u = new URL(url);
+    let basename = u.pathname.split('/').at(-1);
+    return basename;
+}
+
+// Sends the current download state as a browser message. This will be picked up
+// by the popup code if the popup is currently displayed.
+function signal_state() {
+    // this will error out in the promise if the popup
+    // doesn't exist, but we don't care
+    browser.runtime.sendMessage({
+        'action': 'dl_state',
+        'data': downloadState
+    }).catch(() => 0);
+}
+
+function to_filename(str) {
+    return str.toLowerCase().replace(/\s+/g, '_').replace(/[^a-z_]+/g, '');
+}
+
+function Story(url) {
     function chapter_url(story_id, start, end) {
         return `https://fiction.live/api/anonkun/chapters/${story_id}/${start}/${end}`;
     }
@@ -25,7 +46,7 @@ function Story(node_id) {
     // rendering votes, user prompts, etc as HTML.
     function process_html(chapter) {
         let images = [];
-        let chapter_html = "";
+        let all_html = [];
         for (let e of chapter.data) {
             // sometimes a request will include extraneous entries
             // from different chapters
@@ -40,11 +61,11 @@ function Story(node_id) {
                     let $this = $(this);
                     let src = $this.attr('src');
                     images.push(src);
-                    let u = new URL(src);
-                    let basename = u.pathname.split('/').at(-1);
-                    $this.attr('src', basename);
+                    $this.attr('src', '../images/' + url_basename(src));
                 });
-                chapter_html += $dom.prop('outerHTML');
+                $dom.find('figure').find('img').unwrap();
+                $dom.find('img').wrap(`<div class="imgwrap"></div>`);
+                all_html.push($dom.prop('outerHTML'));
             }
             else if (e.nt === 'readerPost') {
                 let title = 'b' in e ? e.b : 'Reader Posts';
@@ -67,7 +88,7 @@ function Story(node_id) {
                     entries.push(`<div class="dice">${dice[k]}</div>`);
                 }
                 html += entries.join('<hr>');
-                chapter_html += `<div class="readerVote">${html}</div>`;
+                all_html.push(`<div class="readerVote">${html}</div>`);
             }
             else if (e.nt === 'choice') {
                 let title = 'b' in e ? e.b : 'Choices';
@@ -75,7 +96,7 @@ function Story(node_id) {
                 let votes = e.choices.map(x => { return { vote: x, count: 0 }; });
                 for (let k in e.votes) {
                     if (Array.isArray(e.votes[k])) {
-                        for (let v in e.votes[k]) {
+                        for (let v of e.votes[k]) {
                             votes[v].count += 1;
                         }
                     }
@@ -85,17 +106,28 @@ function Story(node_id) {
                 }
                 votes.sort((a, b) => b.count - a.count);
                 for (let v of votes) {
-                    html += `<div class="vote">${v.vote}<span class="voteCount">${v.count}</span></div>`;
+                    html += `<div class="vote"><div class="voteText">${v.vote}</div><span class="voteCount">${v.count}</span></div>`;
                 }
-                chapter_html += `<div class="vote">${html}</div>`;
+                all_html.push(`<div class="voteChapter">${html}</div>`);
             }
         }
-        chapter.html = chapter_html;
+        chapter.html = all_html.join('<hr>');
         chapter.images = images;
     }
+
+    function get_node_id(url) {
+        const storyUrlRe = new RegExp('^https://fiction.live/stories/[^/]+/(\\w+)/?');
+        let res = url.match(storyUrlRe);
+        if (res === null) {
+            return;
+        }
+        let nodeId = res[1];
+        return nodeId;
+    }
+
     return {
-        node_id: node_id,
-        download_delay: 2.0,
+        node_id: get_node_id(url),
+        download_delay: 0.5,
         node_metadata: null,
         // This method returns this story's node URL in the API
         node_url: function () {
@@ -104,7 +136,7 @@ function Story(node_id) {
         // This method downloads the node info, returning a promise
         download_node: function () {
             let url = this.node_url();
-            downloadState = { 'stage': 'metadata' };
+            downloadState = { 'stage': 'Getting metadata' };
             return $.get(url).then((data) => {
                 this.node_metadata = data;
                 this.set_chapter_urls();
@@ -170,7 +202,7 @@ function Story(node_id) {
             let num_downloaded = 0;
             let total_to_download = this.chapters.filter(x => (with_special || !x.special)).length;
             let fulfill_func;
-            let chapter_promise = new Promise((resolve, reject) => { fulfill_func = resolve; });
+            let chapter_promise = new Promise((resolve) => { fulfill_func = resolve; });
             let node_md = this.node_metadata;
             let done_processing;
             for (let c of this.chapters) {
@@ -180,16 +212,11 @@ function Story(node_id) {
                 let p = chapter_promise.then(function () {
                     downloadState = {
                         'title': node_md.t,
-                        'stage': 'chapters',
+                        'stage': 'Fetching chapters',
                         'done': num_downloaded,
                         'total': total_to_download
                     };
-                    // this will error out in the promise if the popup
-                    // doesn't exist, but we don't care
-                    browser.runtime.sendMessage({
-                        'action': 'dl_state',
-                        'data': downloadState
-                    }).catch(() => 0);
+                    signal_state();
                     let u = c.url;
                     return $.get(u);
                 });
@@ -207,26 +234,205 @@ function Story(node_id) {
             // done_processing promise, which fires when the final chapter has
             // been downloaded and processed -- the final chapter_promise is
             // just a hanging delay that doesn't have anything chained off it
-            return done_processing.then(() => {
-                downloadState = null;
+            return done_processing;
+        },
+        download_images: function () {
+            // This function relies on all the images being under domains that
+            // ficlivedl has permissions for. Currently the images are hosted at
+            // cdn*.fiction.live, and the extension has permissions for
+            // *.fiction.live. However, this has been different in the past, so
+            // if the image hosting changes again then the extension may break.
+            this.story_images = [];
+            let image_urls = new Set();
+            let fulfill_func;
+            let chapter_promise = new Promise((resolve) => { fulfill_func = resolve; });
+            for (let c of this.chapters) {
+                for (let i of c.images) {
+                    image_urls.add(i);
+                }
+            }
+            this.total_story_images = image_urls.size;
+            let num_downloaded = 0;
+            for (let i of image_urls) {
+                let p = chapter_promise.then(() => {
+                    downloadState = {
+                        'title': this.node_metadata.t,
+                        'stage': 'Fetching images',
+                        'done': num_downloaded,
+                        'total': image_urls.size + 1
+                    };
+                    signal_state();
+                    return $.ajax({
+                        url: i,
+                        xhrFields: {
+                            responseType: 'blob'
+                        }
+                    }).then((data) => {
+                        num_downloaded += 1;
+                        this.story_images.push({
+                            name: url_basename(i),
+                            content: data
+                        });
+                    });
+                });
+                chapter_promise = p;
+            }
+            fulfill_func();
+            return chapter_promise;
+        },
+        download_cover: function () {
+            downloadState = {
+                'title': this.node_metadata.t,
+                'stage': 'Fetching images',
+                'done': this.total_story_images,
+                'total': this.total_story_images + 1
+            };
+            signal_state();
+            let cover_url, cover_name;
+            if ('i' in this.node_metadata) {
+                cover_url = this.node_metadata.i[0];
+                cover_name = url_basename(cover_url);
+            } else {
+                cover_url = "https://placekitten.com/g/800/600";
+                cover_name = "cover.jpg";
+            }
+            return $.ajax({
+                url: cover_url,
+                xhrFields: {
+                    responseType: 'blob'
+                }
+            }).then((data) => {
+                this.cover = {
+                    name: cover_name,
+                    content: data
+                };
+            });
+        },
+        generate_epub: function () {
+            downloadState = {
+                'title': this.node_metadata.t,
+                'stage': 'Generating ePUB file',
+            };
+            signal_state();
+            let metadata = {
+                id: `anonkun:${this.node_id}`,
+                cover: this.cover,
+                title: this.node_metadata.t,
+                author: this.node_metadata.u[0].n,
+                tags: this.node_metadata.ta.join(','),
+                description: this.node_metadata.d,
+                source: url,
+                images: this.story_images
+            };
+            let epub = nodepub.document(metadata);
+            for (let c of this.chapters) {
+                let ctitle = c.metadata.title;
+                if (ctitle.startsWith('#special ')) {
+                    ctitle = ctitle.replace('#special ', 'Appendix: ');
+                }
+                epub.addSection(ctitle, c.html);
+            }
+
+            epub.addCSS(`.vote {
+  display: flex;
+  width: 100%;
+  margin-bottom: 0.4em;
+}
+
+.voteCount {
+  margin-right: 1em;
+  margin-left: auto;
+  text-align: right;
+  align-self: center;
+}
+
+.voteText {
+  max-width: 90%;
+}
+
+.imgwrap {
+  display: flex;
+  justify-content: center;
+  max-width: 100%;
+}
+
+img {
+  max-width: 100%;
+}
+`);
+
+            return epub.getFilesForEPUB().then((files) => {
+                console.log('got epub files');
+                let zip = new JSZip();
+                for (let f of files) {
+                    let path = f.folder !== '' ? `${f.folder}/${f.name}` : f.name;
+                    let opts = {};
+                    // we don't bother compressing image files, they're usually
+                    // already compressed by the format
+                    if (!f.compress || f.folder.indexOf('images') !== -1) {
+                        opts.compression = 'STORE';
+                    }
+                    console.log(f);
+                    zip.file(path, f.content, opts);
+                }
+                console.log('generating zip');
+                let p = zip.generateAsync({
+                    compression: 'DEFLATE',
+                    type: 'blob'
+                }, md => {
+                    downloadState = {
+                        'title': this.node_metadata.t,
+                        'stage': 'Generating ePUB file',
+                        'done': Math.floor(md.percent),
+                        'total': 100
+                    };
+                    signal_state();
+                });
+                return p;
+            }).then((blob) => {
+                console.log('got zip blob');
+                let fn = to_filename(this.node_metadata.t) + '.epub';
+                let blobURL = URL.createObjectURL(blob);
+                return browser.downloads.download({
+                    filename: fn,
+                    saveAs: true,
+                    url: blobURL
+                });
             });
         }
     };
 }
 
-function downloadStory({url, download_special}) {
-    const storyUrlRe = new RegExp('^https://fiction.live/stories/[^/]+/(\\w+)/?');
-    let res = url.match(storyUrlRe);
-    if (res === null) {
-        return;
-    }
-    let nodeId = res[1];
-    let story = Story(nodeId);
+function downloadStory({
+    url,
+    download_special,
+    run_download,
+    download_images,
+    reader_posts
+}) {
+    let story = Story(url);
     story.download_node().then(() => {
         console.log(story.node_metadata);
         return story.download_chapters(download_special);
     }).then(() => {
-        // console.log(story.chapters);
+        if (download_images) {
+            return story.download_images();
+        }
+        return;
+    }).then(() => {
+        return story.download_cover();
+    }
+    ).then(() => {
+        if (run_download) {
+            return story.generate_epub();
+        }
+        return;
+    }).then(() => {
+        downloadState = null;
+        signal_state();
+    }).catch(() => {
+        downloadState = null;
+        signal_state();
     });
 }
 
--- webextension/popup/popup.js
+++ webextension/popup/popup.js
@@ -4,21 +4,60 @@ function handleDownloadClick() {
     if (curTab === null || !('url' in curTab)) {
         return;
     }
-    browser.runtime.sendMessage({
+    let message = {
         action: 'download',
         url: curTab.url,
-        // TODO make this an option
-        download_special: true
-    });
+        download_special: true,
+        run_download: true,
+        download_images: true,
+        reader_posts: true
+    };
+    for (let i of ['download_special', 'run_download', 'download_images', 'reader_posts']) {
+        message[i] = $('#' + i).prop('checked');
+    }
+    console.log(message);
+    browser.runtime.sendMessage(message);
 }
 
 function renderDownloadState(state) {
-    $('#download').hide();
-    $('#dl_state').show();
-    $('#dl_story_title').html(state.title);
-    $('#dl_stage').html(state.stage);
-    $('#dl_done').html(state.done);
-    $('#dl_total').html(state.total);
+    if (state === null) {
+        $('#new_dl').show();
+        $('#dl_state').hide();
+        setup_popup();
+    } else {
+        $('#new_dl').hide();
+        $('#dl_state').show();
+        $('#dl_story_title').html(state.title);
+        $('#dl_stage').html(state.stage);
+        if ('done' in state) {
+            $('#dl_progress').show();
+            $('#dl_done').html(state.done + 1);
+            $('#dl_total').html(state.total);
+        } else {
+            $('#dl_progress').hide();
+        }
+    }
+}
+
+function setup_popup() {
+    $('#download').click(handleDownloadClick);
+    browser.tabs.query({
+        active: true,
+        windowId: browser.windows.WINDOW_ID_CURRENT
+    }).then(function (tabs) {
+        let tab = tabs[0];
+        curTab = tab;
+        if (!('url' in tab)) {
+            // no URL on tab object, therefore it's not a fiction.live
+            // URL (this happens due to permissions: this addon has
+            // only fiction.live host permissions, so the only tabs it
+            // can see URLs for are those from fiction.live)
+            $('#download').prop('disabled', true);
+        }
+        else {
+            $('#download').prop('disabled', false);
+        }
+    });
 }
 
 function handleIconClick() {
@@ -27,24 +66,7 @@ function handleIconClick() {
     }).then(function (resp) {
         let dl_state = resp.state;
         if (dl_state === null) {
-            $('#download').click(handleDownloadClick);
-            browser.tabs.query({
-                active: true,
-                windowId: browser.windows.WINDOW_ID_CURRENT
-            }).then(function (tabs) {
-                let tab = tabs[0];
-                curTab = tab;
-                if (!('url' in tab)) {
-                    // no URL on tab object, therefore it's not a fiction.live
-                    // URL (this happens due to permissions: this addon has
-                    // only fiction.live host permissions, so the only tabs it
-                    // can see URLs for are those from fiction.live)
-                    $('#download').prop('disabled', true);
-                }
-                else {
-                    $('#download').prop('disabled', false);
-                }
-            });
+            setup_popup();
         }
         else {
             renderDownloadState(dl_state);
