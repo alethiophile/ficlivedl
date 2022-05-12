e885f606d8bee967d7e70424ee77af40f1ee2399
alethiophile <mail@alethiophile.dev>
2022-05-12 14:09:11 -0600

    changes for packaging

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ $1 == "dev" ]]; then
-    node_modules/.bin/browserify webextension/background.js >webextension/browser_pack.js
+    node_modules/.bin/browserify --ignore archiver webextension/background.js >webextension/browser_pack.js
 else
-    node_modules/.bin/browserify webextension/background.js --plugin tinyify >webextension/browser_pack.js
+    node_modules/.bin/browserify --ignore archiver  webextension/background.js --plugin tinyify >webextension/browser_pack.js
+    web-ext build -s webextension/ -i jszip.js -i background.js
 fi
