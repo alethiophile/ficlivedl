d33de22e61fcec06c5bea2d06c63a3acbfc52df0
alethiophile <mail@alethiophile.dev>
2022-05-14 21:14:57 -0600

    Setup for building on Chrome-like browsers

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@
 # archiver, so we ignore it
 if [[ $1 == "dev" ]]; then
     node_modules/.bin/browserify --ignore archiver webextension/background.js >webextension/browser_pack.js
+    rm -rf webextension-brave
+    cp -a webextension webextension-brave
+    mv webextension-brave/manifest-brave.json webextension-brave/manifest.json
+    cp node_modules/webextension-polyfill/dist/browser-polyfill.min.js webextension-brave
 else
     node_modules/.bin/browserify --ignore archiver  webextension/background.js --plugin tinyify >webextension/browser_pack.js
     web-ext build -s webextension/ -i jszip.js -i background.js --overwrite-dest
